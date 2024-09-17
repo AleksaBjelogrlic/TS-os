ed64e5b6dc0c265ee315addf98855e5186bc5e51
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-09-17 17:57:07 -0400

    Hello World - First Try
    Won't work with EFI, Laptop won't do legacy BIOS mode. Guess I'll use pixel mode right away, it's what I wanted for the first MVP so why not?

---
+++ multiboot/boot.asm
@@ -0,0 +1,11 @@
+; Taken from 
+; https://os.phil-opp.com/multiboot-kernel/
+
+global start ; Entry point to the kernel
+
+section .text ; default section for executable code 
+bits 32 ; GRUB puts us into protected mode, so 32 bit instructions
+start:
+    ; print `OK` to screen
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
