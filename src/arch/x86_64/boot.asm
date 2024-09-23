49694e7eba497a3eabc56f4ba172cbf1a8da83a6
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-09-23 14:00:10 -0400

    First attempt at some janky assembly

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,12 +1,72 @@
-; Taken from 
-; https://os.phil-opp.com/multiboot-kernel/
+; multiboot info can be found at
+; https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html
 
 global start ; Entry point to the kernel
 
 section .text ; default section for executable code 
 bits 32 ; GRUB puts us into protected mode, so 32 bit instructions
 start:
-    ; print `OK` to screen - won't work with EFI boot :(
-    ; mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+    ; Check to see if we were loaded by a multiboot compatible bootloader
+    ; This is a magic number from the multiboot spec
 
+    cmp eax, 0x36d76289
+    jne error
+
+    ; ebx contains the 32-bit physical address of the 
+    ; Multiboot2 information structure provided by the boot loader  
+
+    mov ecx, [ebx] ; move total_size into ecx
+    add ecx, ebx ; calulate end address of structure 
+    add ebx, 8 ; skip starting tag
+    cmp ebx, ecx ; check if we are past end address
+    jle error ; if we are, go to error
+
+check_type:
+
+    mov edx, [ebx] ; copy data from the address into reg
+    cmp edx, 8 ; check if type is 8 - framebuffer info
+    jne skip_tag ; if type is not 8 jump to skip_tag
+
+parse_fb_info:
+
+    add ebx, 8 ; increment register to point to u64 framebuffer_addr
+    mov edx, [ebx] ; copy framebuffer_addr from the address into reg
+    
+    ;add ebx, 8 ; increment register to point to u32 framebuffer_pitch
+    ;mov ecx, [ebx] ; copy framebuffer_pitch from the address into reg
+    
+    add ebx, 4 ; increment register to point to u32 framebuffer_width
+    mov eax, [ebx] ; copy framebuffer_width from the address into reg
+    add ebx, 4 ; increment register to point to u32 framebuffer_height
+    imul eax, [ebx] ; multiply framebuffer_width by framebuffer_height
+    
+    ;add ebx, 4 ; increment register to point to u32 framebuffer_bpp
+    ;mov r8d, [ebx] ; copy framebuffer_bpp from the address into reg
+
+    imul eax, 4 ; multiply total pixels by 4 bytes per pixel 
+    ; assumes 32-bit truecolour
+
+    mov ebx, edx ; save start address in ebx
+    add edx, eax ; get end address 
+    mov ecx, 0xFFFFFFFF ; pixel colour - all FFs, which is hopefully white
+
+fb_write_loop_hopefully:
+
+    mov [ebx], ecx ; write pixel colour
+    add ebx, 4 ; next pixel
+    cmp ebx, edx ; check if we are past end address
+    jge fb_write_loop_hopefully ; loop if we aren't past end address
+    hlt ; else halt, we're done
+
+
+skip_tag:
+    
+    add ebx, 4 ; increment address to point at u32 size
+    mov edx, [ebx] ; copy tag size data from the address into reg
+    add ebx, edx ; increment address by size
+    cmp ebx, ecx ; check if we are past end address
+    jle error ; if we are, go to error
+    jmp check_type ; else go back to check next tag type
+
+error:
+    hlt
